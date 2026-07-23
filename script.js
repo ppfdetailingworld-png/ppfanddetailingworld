27b8aa3a78d0dcc21e7063e134d963a74059c812
ppfdetailingworld-png <ppfdetailingworld@gmail.com>
2026-07-23 21:35:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
-const galleryImages = document.querySelectorAll(".gallery-box img");
-const lightbox = document.getElementById("lightbox");
-const lightboxImg = document.getElementById("lightbox-img");
-const closeBtn = document.querySelector(".close");
+document.addEventListener("DOMContentLoaded", function () {
 
-galleryImages.forEach((img) => {
-  img.onclick = function () {
-    lightboxImg.src = this.src;
-    lightbox.style.display = "flex";
-};
-      });
+  const galleryImages = document.querySelectorAll(".gallery-box img");
+  const lightbox = document.getElementById("lightbox");
+  const lightboxImg = document.getElementById("lightbox-img");
+  const closeBtn = document.querySelector(".close");
 
-closeBtn.onclick = function () {
-  lightbox.style.display = "none";
-};
+  galleryImages.forEach(img => {
+    img.addEventListener("click", function () {
+      lightboxImg.src = this.src;
+      lightbox.style.display = "flex";
+    });
+  });
 
-lightbox.onclick = function (e) {
-  if (e.target === lightbox) {
+  closeBtn.addEventListener("click", function () {
     lightbox.style.display = "none";
-  }
-};
+  });
+
+  lightbox.addEventListener("click", function (e) {
+    if (e.target === lightbox) {
+      lightbox.style.display = "none";
+    }
+  });
+
+});
