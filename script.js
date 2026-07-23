cf1e75ee2371649920cbc727f93af5d0ae30a9e7
ppfdetailingworld-png <ppfdetailingworld@gmail.com>
2026-07-23 21:12:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 alert("Script Loaded");
 const galleryImages = document.querySelectorAll(".gallery-box img");
+alert(galleryImages.length);
 const lightbox = document.getElementById("lightbox");
 const lightboxImg = document.getElementById("lightbox-img");
 const closeBtn = document.querySelector(".close");
@@ -10,6 +11,7 @@ galleryImages.forEach((img) => {
     lightbox.style.display = "flex";
     lightboxImg.src = this.src;
 };
+      });
 
 closeBtn.onclick = function () {
   lightbox.style.display = "none";
